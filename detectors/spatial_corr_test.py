e6ae8c2b3f8a0df173c80f795e16800f4f5efb93
davv <davide.vecchi@studenti.unitn.it>
2025-12-14 22:19:15 +0100

    df

--- detectors/spatial_corr_test.py
+++ detectors/spatial_corr_test.py
@@ -222,19 +222,19 @@ def process_sample(
                 exp_adv = to_numpy(detector.explain(adv_image, **kwargs))
                 np.save(cache_path_adv, exp_adv)
         
-        if needs_logits:
-            with torch.inference_mode():
-                if pd.isna(df.loc[df_key_orig, "logit"]):
-                    logit_orig = detector.forward(detector.transform(image_np).unsqueeze(0).to('cuda', non_blocking=True)).detach().cpu()
-                    sigmoid_orig = torch.sigmoid(logit_orig).item()
-                    _set_cell(df, df_key_orig, "logit", float(logit_orig))
-                    _set_cell(df, df_key_orig, "sigmoid", float(sigmoid_orig))
-                
-                if pd.isna(df.loc[df_key_adv, "logit"]):
-                    logit_adv = detector.forward(detector.transform(adv_image).unsqueeze(0).to('cuda', non_blocking=True)).detach().cpu()
-                    sigmoid_adv = torch.sigmoid(logit_adv).item()
-                    _set_cell(df, df_key_adv, "logit", float(logit_adv))
-                    _set_cell(df, df_key_adv, "sigmoid", float(sigmoid_adv))
+        # if needs_logits:
+        #     with torch.inference_mode():
+        #         if pd.isna(df.loc[df_key_orig, "logit"]):
+        #             logit_orig = detector.forward(detector.transform(image_np).unsqueeze(0).to('cuda', non_blocking=True)).detach().cpu()
+        #             sigmoid_orig = torch.sigmoid(logit_orig).item()
+        #             _set_cell(df, df_key_orig, "logit", float(logit_orig))
+        #             _set_cell(df, df_key_orig, "sigmoid", float(sigmoid_orig))
+        #
+        #         if pd.isna(df.loc[df_key_adv, "logit"]):
+        #             logit_adv = detector.forward(detector.transform(adv_image).unsqueeze(0).to('cuda', non_blocking=True)).detach().cpu()
+        #             sigmoid_adv = torch.sigmoid(logit_adv).item()
+        #             _set_cell(df, df_key_adv, "logit", float(logit_adv))
+        #             _set_cell(df, df_key_adv, "sigmoid", float(sigmoid_adv))
             
             torch.cuda.empty_cache()
             
