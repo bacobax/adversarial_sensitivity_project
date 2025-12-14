8dd33dd1ff1279e51596c53f6b6325890072da50
davv <davide.vecchi@studenti.unitn.it>
2025-12-14 23:37:16 +0100

    df

--- detectors/spatial_corr_test.py
+++ detectors/spatial_corr_test.py
@@ -59,7 +59,7 @@ LIME_BATCH_SIZE = 256
 LIME_NUM_SAMPLES = 256
 
 cvs_file = 'outputs/results.csv'
-cvs_header = 'detector,attack,category,image,logit,sigmoid,ap,mim'
+cvs_header = 'detector,attack,category,image,logit,prob,ap,mim'
 
 if not os.path.exists(cvs_file):
     with open(cvs_file, 'w') as f:
@@ -229,13 +229,13 @@ def process_sample(
                     logit_orig = detector.forward(detector.transform(image_np).unsqueeze(0).to('cuda', non_blocking=True))
                     sigmoid_orig = torch.sigmoid(logit_orig).item()
                     _set_cell(df, df_key_orig, "logit", float(logit_orig))
-                    _set_cell(df, df_key_orig, "sigmoid", float(sigmoid_orig))
+                    _set_cell(df, df_key_orig, "prob", float(sigmoid_orig))
 
                 if pd.isna(df.loc[df_key_adv, "logit"]):
                     logit_adv = detector.forward(detector.transform(adv_image).unsqueeze(0).to('cuda', non_blocking=True))
                     sigmoid_adv = torch.sigmoid(logit_adv).item()
                     _set_cell(df, df_key_adv, "logit", float(logit_adv))
-                    _set_cell(df, df_key_adv, "sigmoid", float(sigmoid_adv))
+                    _set_cell(df, df_key_adv, "prob", float(sigmoid_adv))
             
                 torch.cuda.empty_cache()
             
