4748f8ecb85367edb0c818af956e8121d5e08d09
Marco Gandolfi <marcogandulf@gmail.com>
2026-01-12 19:46:06 +0100

    cleanup

--- utils/consts.py
+++ utils/consts.py
@@ -11,10 +11,6 @@ IMAGE_EXTENSIONS = {'.jpg', '.jpeg', '.png', '.bmp', '.tiff', '.webp'}
 # Detector configurations
 DETECTOR_MAP = {
     'AnomalyOV': ('AnomalyOVDetector', os.path.join('models', 'anomaly_ov', 'detector.py')),
-    'CLIP-D': ('CLIPDDetector', os.path.join('models', 'CLIP-D', 'detector.py')),
-    'NPR': ('NPRDetector', os.path.join('models', 'NPR', 'detector.py')),
     'R50_nodown': ('R50NoDownDetector', os.path.join('models', 'R50_nodown', 'detector.py')),
-    'R50_TF': ('R50TFDetector', os.path.join('models', 'R50_TF', 'detector.py')),
-    'P2G': ('P2GDetector', os.path.join('models', 'P2G', 'detector.py')),
     'WaveRep': ('WaveRepDetector', os.path.join('models', 'WaveRep', 'detector.py')),
 }
